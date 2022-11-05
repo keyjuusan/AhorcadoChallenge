6f11e8b9da0f294756a11d20f6f33fef8ffdf5d2
Heyker <heykervargas13@gmail.com>
2022-11-05 11:53:52 -0400

    juego terminado

--- code.js
+++ code.js
@@ -6,9 +6,9 @@ agregaPalabras.style.display = "none";
 jugando.style.display = "none";
 
 // ALMACENAJE
-let palabras = ["DUDAS", "CODIGO", "ALURA", "VICTORIA", "DESAFIO"];
+let palabras = ["dudas", "codigo", "alura", "victoria", "desafio"];
 let letrasSub = [];
-let letrasSup = [];
+// let letrasSup;
 
 // AL PRECIONAR el boton "NUEVO JUEGO"
 const btnJugar = document.querySelector(".btnJugar");
@@ -21,72 +21,40 @@ pincel.strokeStyle = "#F9F9F9";
 pincel.lineWidth = "3.5";
 pincel.fillRect(0, 355, 294, 5);
 
-function nuevaPartida() {
+let palabraAleatoria = 0;
+let tecla;
 
-    // VACIAR ARRAY DE LETRAS INCORRECTAS
+function nuevaPartida() {
 
     menuPrincipal.style.display = "none";
     jugando.style.display = "flex";
 
     // AQUI VA EL DESGLOSE DE UNA PALABRA ALEATORIA REPARTIDO EN ESPACIOS LETRA POR LETRA
-    const letrasCorrectas = document.querySelector(".letrasCorrectas");
-    let palabraAleatoria = Math.floor(Math.random() * palabras.length);
-
-
-    for (var i = 0; i < palabras[palabraAleatoria].length; i++) {
-
-        const cajaLetra = document.createElement("div");
-        cajaLetra.classList.add("cajaLetra");
-        letrasCorrectas.appendChild(cajaLetra);
-        const letra = document.createElement("p");
-        letra.classList.add("p" + i);
-        letra.style.display = "none";
-        letra.textContent = palabras[palabraAleatoria].charAt(i).toUpperCase();
-        cajaLetra.appendChild(letra);
-    }
+    palabraAzar();
 
     const letrasIncorrectas = document.querySelector(".letrasIncorrectas");
     document.addEventListener("keydown", function paco(event) {
         let encontrado = false;
-        let encontra2 = false;
 
-        // REVELA LAS LETRAS CORRECTAS
-        for (var g = 0; g < palabras[palabraAleatoria].length; g++) {
-            let verLetra = document.querySelector(".p" + g);
-            if (event.key.toUpperCase() == palabras[palabraAleatoria].charAt(g))
-                verLetra.style.display = "block";
-        }
+        const cajaLetra = document.querySelectorAll('.cajaLetra')
 
-        for (var j = 0; j < 8; j++) {
-            if (event.key.toUpperCase() == letrasSub[j] || event.key.toUpperCase() == palabras[palabraAleatoria].charAt(j)) {
-
+        for (let h = 0; h < palabras[palabraAleatoria].length; h++) {
+            if (event.key == palabras[palabraAleatoria].charAt(h)) {
+                cajaLetra[h].innerHTML = `<p>${palabras[palabraAleatoria].charAt(h).toUpperCase()}</p>`;
                 encontrado = true;
-                break;
+            }
+        }
+
+        for (let j = 0; j < palabras[palabraAleatoria].length; j++) {
+            if (event.key.toLowerCase() == letrasSub[j]) {
+                encontrado = true;
             } else {
-                console.log("pasó algo");
+                // console.log("pasó algo");
             }
-        }
-
-        // VALIDACION PARA IDENTIFICAR CUANDO SE GANA EN EL JUEGO
-        for (var h = 0; h < palabras[palabraAleatoria].length; h++) {
-            if (event.key.toUpperCase() == palabras[palabraAleatoria].charAt(h)) {
-                encontra2 = true;
-            } else {  
-                letrasSup.push(event.key.toUpperCase());
-                break;
-            }
-        }
-
-
-        if (letrasSup.length == palabras[palabraAleatoria].length) {
-            pincel.font = "24px Monospace";
-            pincel.fillStyle = "#7DCE13";
-            pincel.fillText("¡Ganaste!", 135, 280);
-            document.removeEventListener("keydown", paco)
         }
 
         if (encontrado == false) {
-            letrasSub.push(event.key.toUpperCase());
+            letrasSub.push(event.key.toLowerCase());
             letrasIncorrectas.innerHTML += "<p>" + event.key.toUpperCase() + "</p>";
 
             if (letrasSub.length === 1) {
@@ -130,13 +98,108 @@ function nuevaPartida() {
                 pincel.font = "24px Monospace";
                 pincel.fillStyle = "#FF1E00";
                 pincel.fillText("¡Perdiste!", 135, 280);
-                document.removeEventListener("keydown", paco)
+                document.removeEventListener("keydown", paco);
+                for(let i=0;i<27;i++){tecla[i].disabled = true;}
             }
 
+        } else if (document.querySelectorAll('.cajaLetra p').length == palabras[palabraAleatoria].length) {
+            pincel.font = "24px Monospace";
+            pincel.fillStyle = "#7DCE13";
+            pincel.fillText("¡Ganaste!", 135, 280);
+            document.removeEventListener("keydown", paco);
+            for(let i=0;i<27;i++){tecla[i].disabled = true;}
+            
         } else {
-            console.log("problemon");
+            // console.log("problemon");
         }
     });
+
+// PARTE LOGICA DEL TECLADO
+
+    for (let k = 0; k < 27; k++) {
+        tecla = document.querySelectorAll('.teclado button');
+        tecla[k].addEventListener('click', function pacoKeys(event) {
+            // console.log(event.target.innerHTML)
+
+            let encontrado = false;
+
+            const cajaLetra = document.querySelectorAll('.cajaLetra')
+
+            for (let h = 0; h < palabras[palabraAleatoria].length; h++) {
+                if (event.target.innerHTML.toLowerCase() == palabras[palabraAleatoria].charAt(h)) {
+                    cajaLetra[h].innerHTML = `<p>${palabras[palabraAleatoria].charAt(h).toUpperCase()}</p>`;
+                    encontrado = true;
+                }
+            }
+
+            for (let j = 0; j < palabras[palabraAleatoria].length; j++) {
+                if (event.target.innerHTML.toLowerCase() == letrasSub[j]) {
+                    encontrado = true;
+                } else {
+                    // console.log("pasó algo");
+                }
+            }
+
+            if (encontrado == false) {
+                letrasSub.push(event.target.innerHTML.toLowerCase());
+                letrasIncorrectas.innerHTML += "<p>" + event.target.innerHTML.toUpperCase() + "</p>";
+
+                if (letrasSub.length === 1) {
+                    //palo vertical
+                    crearRectangulo(98, 0, 5, 360, "#F9F9F9");
+
+                } else if (letrasSub.length === 2) {
+                    //palo horizontal
+                    crearRectangulo(98, 0, 156, 3.5, "#F9F9F9");
+
+                } else if (letrasSub.length === 3) {
+                    //cuerda
+                    crearRectangulo(250.5, 0, 3.5, 55, "#F9F9F9");
+
+                } else if (letrasSub.length === 4) {
+                    //cabeza
+                    crearCirculo(253, 81, 25, "#F9F9F9");
+
+                } else if (letrasSub.length === 5) {
+                    //soga en el cuello
+                    crearLinea(248, 110, 258, 110, "3.5", "#F9F9F9");
+                    //torso
+                    crearRectangulo(251.5, 112, 3.5, 80, "#F9F9F9");
+
+                } else if (letrasSub.length === 6) {
+                    //brazo derecho
+                    crearLinea(254, 122, 282, 152, "3.5", "#F9F9F9");
+
+                } else if (letrasSub.length === 7) {
+                    //brazo izquierdo
+                    crearLinea(252, 122.5, 224, 152, "3.5", "#F9F9F9");
+
+                } else if (letrasSub.length === 8) {
+                    //pierna derecha
+                    crearLinea(254, 192, 282, 225, "3.5", "#F9F9F9");
+
+                } else if (letrasSub.length === 9) {
+                    //pierna izquierda
+                    crearLinea(252, 192, 224, 225, "3.5", "#F9F9F9");
+
+                    pincel.font = "24px Monospace";
+                    pincel.fillStyle = "#FF1E00";
+                    pincel.fillText("¡Perdiste!", 135, 280);
+                    for(let i=0;i<27;i++){tecla[i].disabled = true;}
+                    
+                }
+
+            } else if (document.querySelectorAll('.cajaLetra p').length == palabras[palabraAleatoria].length) {
+                pincel.font = "24px Monospace";
+                pincel.fillStyle = "#7DCE13";
+                pincel.fillText("¡Ganaste!", 135, 280);
+                for(let i=0;i<27;i++){tecla[i].disabled = true;}
+            } else {
+                // console.log("problemon");
+            }
+
+        });
+    }
 
 }
 
@@ -163,11 +226,17 @@ function crearLinea(x1, y1, x2, y2, grosor, color) {
 
 btnJugar.addEventListener("click", nuevaPartida);
 
+function palabraAzar() {
+    const letrasCorrectas = document.querySelector(".letrasCorrectas");
+    palabraAleatoria = Math.floor(Math.random() * palabras.length);
 
-// const btnNuevoJuego = document.querySelector(".btnNuevoJuego");
-// btnNuevoJuego.addEventListener("click", () => {
-//     location.reload();
-// });
+
+    for (let i = 0; i < palabras[palabraAleatoria].length; i++) {
+        const cajaLetra = document.createElement("div");
+        cajaLetra.classList.add("cajaLetra");
+        letrasCorrectas.appendChild(cajaLetra);
+    }
+};
 
 const btnRendirse = document.querySelector(".btnRendirse");
 btnRendirse.addEventListener("click", () => {
